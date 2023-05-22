a4434aae1fce34556b17ed4b6e110c4ee513c3b1
Aiden <azizjalelcontact@gmail.com>
2023-05-22 16:24:30 -0500

    d

--- app/getApiDataLastWeek.tsx
+++ app/getApiDataLastWeek.tsx
@@ -1,7 +1,7 @@
 import { Batch } from "./datatable/columns";
 
 export async function getApiDataLastWeek(): Promise<Batch[]> {
-  const batchSize = 2500; // set batch size for pagination
+  const batchSize = 10000; // set batch size for pagination
   const now = new Date();
   const sevenDaysAgo = new Date(now.getTime() - 162 * 60 * 60 * 1000);
   const firstTradeTimestamp = Math.floor(sevenDaysAgo.getTime() / 1000);
