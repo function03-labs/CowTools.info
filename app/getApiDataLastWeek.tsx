57e8da8dac71944fe3353f5bb0e258db6c5a94eb
Aiden <azizjalelcontact@gmail.com>
2023-05-22 16:22:20 -0500

    x

--- app/getApiDataLastWeek.tsx
+++ app/getApiDataLastWeek.tsx
@@ -1,29 +1,15 @@
 import { Batch } from "./datatable/columns";
-import chalk from "chalk";
 
 export async function getApiDataLastWeek(): Promise<Batch[]> {
-  const batchSize = 1000; // set batch size for pagination
+  const batchSize = 2500; // set batch size for pagination
   const now = new Date();
   const sevenDaysAgo = new Date(now.getTime() - 162 * 60 * 60 * 1000);
   const firstTradeTimestamp = Math.floor(sevenDaysAgo.getTime() / 1000);
 
-  const promises: Promise<any>[] = [];
-  console.log(chalk.yellow("Starting API call long calls"));
+  let page = 0;
+  let allData: Batch[] = [];
 
-  fetch(
-    "https://webhook.site/25de2bc8-038b-44d1-a33d-cbaae96afdb6",
-    {
-      next: { revalidate: 24 * 60 * 60 },
-
-      method: "POST",
-
-      body: JSON.stringify({
-        "time": "lastweek"
-      }),
-    }
-  );
-
-  for (let page = 0; page < 20; page++) {
+  while (true) {
     const data = JSON.stringify({
       collection: process.env.MONGODB_COLLECTION_NAME,
       database: process.env.MONGODB_DB_NAME,
@@ -37,41 +23,38 @@ export async function getApiDataLastWeek(): Promise<Batch[]> {
       limit: batchSize,
       skip: page * batchSize,
     });
-    console.log(chalk.yellow(`Fetching data for page ${page}`));
+    //make a call to this api and send lastweek data post
+    //https://webhook.site/25de2bc8-038b-44d1-a33d-cbaae96afdb6
 
-    promises.push(
-      fetch(
+    try {
+      const response = await fetch(
         "https://us-east-2.aws.data.mongodb-api.com/app/data-fnjyq/endpoint/data/v1/action/find",
         {
           next: { revalidate: 24 * 60 * 60 },
+          method: "POST",
           headers: {
             "Content-Type": "application/json",
             "Access-Control-Request-Headers": "*",
-            "api-key": "BVoQG8gP315yFGK3Jnr73DLU4HPj7bIC6fUo9eqbhrNMtDuaKu5dWLppRG9i7Mer",
+            "api-key": "5KHg7ImnBlNQlXkGGbyyB4LFoN0g9hk4fxUdWJbyKdd1bxo3DDrr48YjCHQquWMG",
           },
-          method: "POST",
           body: data,
         }
+      );
 
-      )
-        .then((response) => response.json())
-        .then((responseData) => responseData.documents as unknown as Batch[])
-        .catch((error) => {
-          console.log(chalk.red(error));
-          return [];
-        })
-    );
+      const responseData = await response.json();
+      console.log(responseData.documents.length, "data historical api");
+
+      if (responseData.documents.length === 0) {
+        break; // break out of loop if no more data
+      }
+
+      allData.push(...(responseData.documents as unknown as Batch[]));
+      page++;
+    } catch (error) {
+      console.log(error);
+      break;
+    }
   }
-  console.log(chalk.yellow("Waiting for all promises to resolve"));
-
-  const allData = await Promise.all(promises).then((results) =>
-    results.reduce((acc, val) => acc.concat(val), [])
-  );
-
-  console.log(
-    chalk.green(allData.length),
-    chalk.blue("data historical api")
-  );
 
   return allData;
 }
