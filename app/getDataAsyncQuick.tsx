df0180346a0bb469530be6d1d37f018706e2ba47
Aiden <azizjalelcontact@gmail.com>
2023-05-22 11:27:02 -0500

    caching

--- app/getDataAsyncQuick.tsx
+++ app/getDataAsyncQuick.tsx
@@ -32,7 +32,7 @@ export async function getDataAsyncQuick(): Promise<Batch[]> {
       const response = await fetch(
         "https://us-east-2.aws.data.mongodb-api.com/app/data-fnjyq/endpoint/data/v1/action/find",
         {
-          next: { revalidate: 30 },
+          next: { revalidate: 0 },
           method: "POST",
           headers: {
             "Content-Type": "application/json",
