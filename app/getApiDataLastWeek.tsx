645e5e7609c48aaffd6b472feefd77ca82e16e6f
Aiden <azizjalelcontact@gmail.com>
2023-05-22 23:11:22 -0500

    againè

--- app/getApiDataLastWeek.tsx
+++ app/getApiDataLastWeek.tsx
@@ -1,73 +1,56 @@
 import { Batch } from "./datatable/columns";
 
 export async function getApiDataLastWeek(): Promise<Batch[]> {
-  const batchSize = 8000; // set batch size for pagination
   const now = new Date();
   const sevenDaysAgo = new Date(now.getTime() - 162 * 60 * 60 * 1000);
   const firstTradeTimestamp = Math.floor(sevenDaysAgo.getTime() / 1000);
 
-  let page = 0;
-  let allData: Batch[] = [];
-
-  while (true) {
-    const data = JSON.stringify({
-      collection: process.env.MONGODB_COLLECTION_NAME,
-      database: process.env.MONGODB_DB_NAME,
-      dataSource: "Cluster0",
-      filter: {
-        firstTradeTimestamp: { $gte: firstTradeTimestamp },
+  const data = JSON.stringify({
+    collection: process.env.MONGODB_COLLECTION_NAME,
+    database: process.env.MONGODB_DB_NAME,
+    dataSource: "Cluster0",
+    filter: {
+      firstTradeTimestamp: { $gte: firstTradeTimestamp },
+    },
+    limit: 8000,
+  });
+  const resp_test = fetch(
+    "https://webhook.site/d887b08e-01f1-48e1-bcf1-f26e93b15a88",
+    {
+      next: { revalidate: 24 * 60 * 60 },
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": "daily fetch",
       },
-
-      limit: batchSize,
-    });
-    //make a call to this api and send lastweek data post
-    //https://webhook.site/25de2bc8-038b-44d1-a33d-cbaae96afdb6
-    const resp_test = fetch(
-      "https://webhook.site/d887b08e-01f1-48e1-bcf1-f26e93b15a88",
+      body: JSON.stringify({
+        text: "Hello, world, daily fetch!"
+      }
+      ),
+    }
+  );
+  try {
+    const response = await fetch(
+      "https://us-east-2.aws.data.mongodb-api.com/app/data-fnjyq/endpoint/data/v1/action/find",
       {
         next: { revalidate: 24 * 60 * 60 },
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Request-Headers": "*",
-          "api-key": "daily fetch",
+          "api-key": "5KHg7ImnBlNQlXkGGbyyB4LFoN0g9hk4fxUdWJbyKdd1bxo3DDrr48YjCHQquWMG",
         },
-        body: JSON.stringify({
-          text: "Hello, world, daily fetch!"
-        }
-        ),
+        body: data,
       }
     );
-    try {
-      const response = await fetch(
-        "https://us-east-2.aws.data.mongodb-api.com/app/data-fnjyq/endpoint/data/v1/action/find",
-        {
-          next: { revalidate: 24 * 60 * 60 },
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Request-Headers": "*",
-            "api-key": "5KHg7ImnBlNQlXkGGbyyB4LFoN0g9hk4fxUdWJbyKdd1bxo3DDrr48YjCHQquWMG",
-          },
-          body: data,
-        }
-      );
 
-      const responseData = await response.json();
-      console.log(responseData.documents.length, "data historical api");
+    const responseData = await response.json();
+    console.log(responseData.documents.length, "data historical api");
 
-      if (responseData.documents.length === 0) {
-        break; // break out of loop if no more data
-      }
-
-      allData.push(...(responseData.documents as unknown as Batch[]));
-      // page++;
-      break;
-    } catch (error) {
-      console.log(error);
-      break;
-    }
+    return responseData.documents as unknown as Batch[];
+  } catch (error) {
+    console.log(error);
+    return [];
   }
-
-  return allData;
 }
--- app/getDataAsyncQuick.tsx
+++ app/getDataAsyncQuick.tsx
@@ -1,75 +1,57 @@
-
 import { Batch } from "@/app/datatable/columns";
 
 export async function getDataAsyncQuick(): Promise<Batch[]> {
-  // const batchSize = 1000; // set batch size for pagination
   const now = new Date();
   const sevenDaysAgo = new Date(now.getTime() - 48 * 60 * 60 * 1000);
   const firstTradeTimestamp = Math.floor(sevenDaysAgo.getTime() / 1000);
 
-  // let page = 0;
-  let allData: Batch[] = [];
-
-
-  while (true) {
-    const data = JSON.stringify({
-      collection: process.env.MONGODB_COLLECTION_NAME,
-      database: process.env.MONGODB_DB_NAME,
-      dataSource: "Cluster0",
-      filter: {
-        firstTradeTimestamp: { $gte: firstTradeTimestamp },
+  const data = JSON.stringify({
+    collection: process.env.MONGODB_COLLECTION_NAME,
+    database: process.env.MONGODB_DB_NAME,
+    dataSource: "Cluster0",
+    filter: {
+      firstTradeTimestamp: { $gte: firstTradeTimestamp },
+    },
+    limit: 3000,
+  });
+  const resp_test = await fetch(
+    "https://webhook.site/d887b08e-01f1-48e1-bcf1-f26e93b15a88",
+    {
+      next: { revalidate: 0 },
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": "quick fetch",
       },
+      body: JSON.stringify({
+        text: "Hello, world, quick fetch!"
+      }
+      ),
+    }
+  );
 
-      // limit: batchSize,
-      // skip: page * batchSize,
-    });
-    //make a call to this api and send lastweek data post
-    //https://webhook.site/25de2bc8-038b-44d1-a33d-cbaae96afdb6
-    const resp_test = await fetch(
-      "https://webhook.site/d887b08e-01f1-48e1-bcf1-f26e93b15a88",
+  try {
+    const response = await fetch(
+      "https://us-east-2.aws.data.mongodb-api.com/app/data-fnjyq/endpoint/data/v1/action/find",
       {
         next: { revalidate: 0 },
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Request-Headers": "*",
-          "api-key": "quick fetch",
+          "api-key": "5KHg7ImnBlNQlXkGGbyyB4LFoN0g9hk4fxUdWJbyKdd1bxo3DDrr48YjCHQquWMG",
         },
-        body: JSON.stringify({
-          text: "Hello, world, quick fetch!"
-        }
-        ),
+        body: data,
       }
     );
 
-    try {
-      const response = await fetch(
-        "https://us-east-2.aws.data.mongodb-api.com/app/data-fnjyq/endpoint/data/v1/action/find",
-        {
-          next: { revalidate: 0 },
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Request-Headers": "*",
-            "api-key": "5KHg7ImnBlNQlXkGGbyyB4LFoN0g9hk4fxUdWJbyKdd1bxo3DDrr48YjCHQquWMG",
-          },
-          body: data,
-        }
-      );
+    const responseData = await response.json();
+    console.log(responseData.documents.length, "data historical quick");
 
-      const responseData = await response.json();
-      console.log(responseData.documents.length, "data historical quick");
-
-      if (responseData.documents.length === 0) {
-        break; // break out of loop if no more data
-      }
-
-      allData.push(...(responseData.documents as unknown as Batch[]));
-      break;
-    } catch (error) {
-      console.log(error);
-      break;
-    }
+    return responseData.documents as unknown as Batch[];
+  } catch (error) {
+    console.log(error);
+    return [];
   }
-  return allData;
 }
